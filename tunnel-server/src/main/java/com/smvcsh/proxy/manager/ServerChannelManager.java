2f45830d514ee9c15a998dc9026aac7c352aef7a
zhaochengtao <zhaochengtao@thunisoft.com>
2023-07-11 15:30:38 +0800

    补充提交

---
+++ tunnel-server/src/main/java/com/smvcsh/proxy/manager/ServerChannelManager.java
@@ -0,0 +1,9 @@
+package com.smvcsh.proxy.manager;
+
+import com.smvcsh.proxy.manager.ChannelManager;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServerChannelManager extends ChannelManager {
+	
+}
