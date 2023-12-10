45cc4f53ff9064820f59dc1b3d9207ecc1309fd9
taotao <taotao@github.com>
2023-12-10 11:10:06 +0800

    feat：单客户端多server支持

--- tunnel-client/src/main/java/com/smvcsh/proxy/manager/ClientChannelManager.java
+++ tunnel-client/src/main/java/com/smvcsh/proxy/manager/ClientChannelManager.java
@@ -1,21 +1,35 @@
 package com.smvcsh.proxy.manager;
 
 import com.smvcsh.proxy.client.ProxyTcpBusClient;
+import com.smvcsh.proxy.client.ProxyTcpDataClient;
 import com.smvcsh.proxy.manager.ChannelManager;
 import io.netty.channel.ChannelFuture;
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
-@Component
+//@Component
+@Data
 public class ClientChannelManager extends ChannelManager {
+
+	private String host;
+	private int port;
+	private boolean starting;
 	
-	@Resource
+//	@Resource
 	private ProxyTcpBusClient busClient;
+	private ProxyTcpDataClient dataClient;
+
+	public ClientChannelManager(String host, int port) {
+		this.busClient = new ProxyTcpBusClient(this);
+		this.dataClient = new ProxyTcpDataClient(this);
+		this.host = host;
+		this.port = port;
+	}
 
 	public ChannelFuture busConnect(String host, int port) {
 		// TODO Auto-generated method stub
 		return busClient.connect(host, port);
 	}
-	
 }
