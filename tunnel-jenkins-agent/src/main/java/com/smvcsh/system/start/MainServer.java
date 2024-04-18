d3e4d16c34ada709e3417137960a840b888bd2d3
zhaochengtao <zhaochengtao@thunisoft.com>
2024-04-18 12:37:09 +0800

    feat:add jenkins agent

--- tunnel-jenkins-agent/src/main/java/com/smvcsh/system/start/MainServer.java
+++ tunnel-jenkins-agent/src/main/java/com/smvcsh/system/start/MainServer.java
@@ -1,6 +1,5 @@
 package com.smvcsh.system.start;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,7 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 //@EnableAutoConfiguration
-@ComponentScan(basePackages = {"com.smvcsh.proxy.**", "com.smvcsh.system.config"})
+@ComponentScan(basePackages = {"com.smvcsh.proxy.**", "com.smvcsh.system.config", "com.thunisoft.tools.tunnel.jenkins.agent"})
 @EnableScheduling
 public class MainServer {
     public static void main(String[] args) {
